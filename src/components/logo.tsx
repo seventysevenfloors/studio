8ab1471687b8f05e3fd06f09517cdaf2f7431c26
lelouch <lelouch19113@gmail.com>
2026-02-19 17:50:55 +0000

    not a single imagd is displayed

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,11 +1,14 @@
+import Image from 'next/image';
+
 export function Logo() {
   return (
-    <img
+    <Image
       src="/logo.jpg"
       alt="Ar Constructions and Real Estate Logo"
       width="190"
       height="42"
       className="object-contain"
+      priority
     />
   );
 }
