cc456f93ca4b63d021c3063a8b64fece6b663d48
lelouch <lelouch19113@gmail.com>
2026-02-19 17:24:02 +0000

    i think problems still exist. carefully change paths

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,14 +1,11 @@
-import Image from 'next/image';
-
 export function Logo() {
   return (
-    <Image
+    <img
       src="/logo.jpg"
       alt="Ar Constructions and Real Estate Logo"
-      width={190}
-      height={42}
+      width="190"
+      height="42"
       className="object-contain"
-      priority
     />
   );
 }
