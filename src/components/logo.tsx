bbab5af51b3c074943c2cb6c097efbdedab5b83d
lelouch <lelouch19113@gmail.com>
2026-02-19 18:16:00 +0000

    replace gallery images extension from jpg to jpeg

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 export function Logo() {
   return (
     <Image
-      src="/logo.jpg"
+      src="/logo.jpeg"
       alt="Ar Constructions and Real Estate Logo"
       width="190"
       height="42"
