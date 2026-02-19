0eec0743174e7550c6a80af1f7cf6ef7f2d37c23
lelouch <lelouch19113@gmail.com>
2026-02-19 18:38:16 +0000

    replace logo.jpeg with logo-2.jpeg

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 export function Logo() {
   return (
     <Image
-      src="/logo.jpg"
+      src="/logo-2.jpeg"
       alt="Ar Constructions and Real Estate Logo"
       width="190"
       height="42"
